55021a3a67c0644e9a40f1c5540d223bceb3cb91
Ian Mascarenhas <dev@moveandfire.co.uk>
2020-05-15 19:40:21 +0100

    Added blue colour for negative prices in agle rates card

--- custom_cards/agile-rates-card.js
+++ custom_cards/agile-rates-card.js
@@ -67,6 +67,10 @@ class AgileRatesCard extends HTMLElement {
                 border: 2px solid MediumSeaGreen;
                 background-color: MediumSeaGreen;
             }
+            td.blue {
+                border: 2px solid #391CD9;
+                background-color: #391CD9;
+            }
             `;
             card.appendChild(style);
             card.appendChild(this.content);
@@ -96,6 +100,7 @@ class AgileRatesCard extends HTMLElement {
             var colour = "green";
             if(attributes[key] > highlimit) colour = "red";
             else if(attributes[key] > mediumlimit) colour = "orange";
+            else if(attributes[key] <= 0 ) colour = "blue";
             table = table.concat("<tr class='rate_row'><td class='time time_"+colour+"'>" + time_locale + "</td><td class='rate "+colour+"'>" + attributes[key] + unitstr + "</td></tr>");
             if (x % rows_per_col == 0) {
                 tables = tables.concat(table);
